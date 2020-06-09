d2f95e5c3ee4c18e4057437dd6eaac10c2ea2f54
Jos33y <josephlagbalu@gmail.com>
2020-06-09 15:47:35 +0100

    understanding android application basics

---
+++ app/src/test/java/com/example/firstapp/DataManagerTest.java
@@ -0,0 +1,13 @@
+package com.example.firstapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DataManagerTest {
+
+    @Test
+    public void createNewNote() {
+
+    }
+}
